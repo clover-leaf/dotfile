6628b8087a2e47376967c2bdd3978f30534b53a9
thitthan <thangnguyenmatt@gmail.com>
2025-11-08 23:37:57 +0200

    feat(nvim): add flash plugin for navigate with label

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -182,6 +182,18 @@ require("lazy").setup({
     event = "BufReadPre",
     opts = { -- set to setup table
     },
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "th", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "tH", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   }
   --  {
   --    "yetone/avante.nvim",
